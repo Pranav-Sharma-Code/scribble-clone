a90312e4701d9488be946f7eb6ac6b58be8a735b
Pranav-Sharma-Code <pranavofficial2006@gmail.com>
2026-06-01 02:27:34 +0530

    Deployment

--- server/index.js
+++ server/index.js
@@ -9,22 +9,26 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Scribble Backend Running");
+});
+
 const server = http.createServer(app);
+
+const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: allowedOrigin,
         methods: ["GET", "POST"],
     },
 })
 
-
-
 io.on("connection", (socket) => {
-    console.log("USER CONNECTED:", socket.id);
     socketHandler(io, socket);
 });
+const PORT = process.env.PORT || 3001;
 
-server.listen(3001, () => {
-    console.log("SERVER RUNNING ON 3001");
+server.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON ${PORT}`);
 });
